f329a69366fa1a272fb78d244a439a56e577e38d
TSlump <tslump@vqcomms.com>
2024-12-02 09:24:46 +0000

    Part One

--- AOC Day 1/Program.cs
+++ AOC Day 1/Program.cs
@@ -1,3 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.IO;
+using System.Linq;
 
-Console.WriteLine("Hello, World!");
+var list1 = new List<int>();
+var list2 = new List<int>();
+
+var filePath = "Day1Input.txt";
+
+if (File.Exists(filePath))
+{
+    foreach (var line in File.ReadLines(filePath))
+    {
+        var locationIds = line.Split("   ");
+        
+        list1.Add(Convert.ToInt32(locationIds[0]));
+        list2.Add(Convert.ToInt32(locationIds[1]));
+    }
+}
+else
+{
+    Console.WriteLine("File not found: " + filePath);
+}
+
+list1.Sort();
+list2.Sort();
+
+var listDifference = 0;
+
+if (list1.Count == list2.Count)
+{
+    for (var i = 0; i < list1.Count; i++)
+    {
+        listDifference += Math.Abs(list1[i] - list2[i]);
+    }
+}
+
+Console.WriteLine(listDifference);
