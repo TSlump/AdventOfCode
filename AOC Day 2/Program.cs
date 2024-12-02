ce7d9d66485e41472c3124ea3952284a6db27282
TSlump <tslump@vqcomms.com>
2024-12-02 14:53:44 +0000

    Day 2

---
+++ AOC Day 2/Program.cs
@@ -0,0 +1,146 @@
+﻿var filePath = "Day2Input.txt";
+
+var safeRecords = 0;
+
+if (File.Exists(filePath))
+{
+    foreach (var line in File.ReadLines(filePath))
+    {
+        var levels = new List<string>(line.Split(" "));
+
+        if (IsSafelyIncreasing(levels) || IsSafelyDecreasing(levels))
+        {
+            safeRecords++;
+        }
+        
+    }
+}
+else
+{
+    Console.WriteLine("File not found: " + filePath);
+}
+
+Console.WriteLine($"Safe Records: {safeRecords}");
+
+
+// Part 2
+
+safeRecords = 0;
+
+if (File.Exists(filePath))
+{
+    foreach (var line in File.ReadLines(filePath))
+    {
+        var levels = new List<string>(line.Split(" "));
+
+        if (IsSafelyIncreasingWithDampener(levels) || IsSafelyDecreasingWithDampener(levels))
+        {
+            safeRecords++;
+        }
+        
+    }
+}
+else
+{
+    Console.WriteLine("File not found: " + filePath);
+}
+
+Console.WriteLine($"Safe Records with Problem Dampener: {safeRecords}");
+
+return;
+
+bool IsSafelyIncreasing(List<string> levels)
+{
+    if (levels.Count < 2)
+    {
+        return false;
+    }
+    
+    var prev = 0;
+    
+    for (var i = 0; i < levels.Count; i++)
+    {
+        if (i == 0)
+        {
+            prev = Convert.ToInt32(levels[i]);
+        }
+        else
+        {
+            var current  = Convert.ToInt32(levels[i]);
+            if (!(prev < current && current - prev < 4))
+            {
+                return false;
+            }
+            
+            prev = current;
+        }
+    }
+    
+    return true;
+}
+
+bool IsSafelyDecreasing(List<string> levels)
+{
+    if (levels.Count < 2)
+    {
+        return false;
+    }
+    
+    var prev = 0;
+    
+    for (var i = 0; i < levels.Count; i++)
+    {
+        if (i == 0)
+        {
+            prev = Convert.ToInt32(levels[i]);
+        }
+        else
+        {
+            var current  = Convert.ToInt32(levels[i]);
+            if (!(prev > current && prev - current < 4))
+            {
+                return false;
+            }
+            
+            prev = current;
+        }
+    }
+    
+    return true;
+}
+
+bool IsSafelyIncreasingWithDampener(List<string> levels)
+{
+    if (IsSafelyIncreasing(levels))
+    {
+        return true;
+    }
+
+    for (var i = 0; i < levels.Count; i++)
+    {
+        if (IsSafelyIncreasing(levels.Where((item, index) => index != i).ToList()))
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+bool IsSafelyDecreasingWithDampener(List<string> levels)
+{
+    if (IsSafelyDecreasing(levels))
+    {
+        return true;
+    }
+
+    for (var i = 0; i < levels.Count; i++)
+    {
+        if (IsSafelyDecreasing(levels.Where((item, index) => index != i).ToList()))
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
